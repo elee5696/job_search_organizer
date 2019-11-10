d026879b4f3e45038fd2544d18413e7b25d47cc3
Edward W. Lee <elee5696@gmail.com>
2019-11-10 12:54:45 -0800

    corrected table HTML

--- client/components/post/post-table.jsx
+++ client/components/post/post-table.jsx
@@ -7,23 +7,27 @@ export default class PostTable extends React.Component {
     return (
       <div className="post container">
         <table className="post-table">
-          <tr>
-            <PostTableHeader sort={this.props.sort} value="Company" field="company" />
-            <PostTableHeader sort={this.props.sort} value="Applied" field="date_applied" />
-            <PostTableHeader sort={this.props.sort} value="Response" field="response_date" />
-            <th align="left">Functions</th>
-          </tr>
-          {
-            this.props.jobs.map(job => {
-              return (
-                <Post
-                  key={job.id}
-                  job={job}
-                  deleteModalView={this.props.deleteModalView}
-                  callbacks={{ edit: this.props.edit, delete: this.props.delete, toggleDelete: this.props.toggleDeleteModal }} />
-              );
-            })
-          }
+          <thead>
+            <tr>
+              <PostTableHeader sort={this.props.sort} value="Company" field="company" />
+              <PostTableHeader sort={this.props.sort} value="Applied" field="date_applied" />
+              <PostTableHeader sort={this.props.sort} value="Response" field="response_date" />
+              <th align="left">Functions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.props.jobs.map(job => {
+                return (
+                  <Post
+                    key={job.id}
+                    job={job}
+                    deleteModalView={this.props.deleteModalView}
+                    callbacks={{ edit: this.props.edit, delete: this.props.delete, toggleDelete: this.props.toggleDeleteModal }} />
+                );
+              })
+            }
+          </tbody>
         </table>
       </div>
     );
